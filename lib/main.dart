2f16973826d719ae003ad4f69b6dc352c0628579
José Ferreira <joseguedes.dev@gmail.com>
2024-05-21 23:12:50 -0300

    Update NavBar color

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.brown,
         title: Text(widget.title),
       ),
       body: SafeArea(
